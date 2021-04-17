5449e4ed73bcab7abb27e0930200a6a7fbfa39e2
Viktor <67110882+JadeBerg@users.noreply.github.com>
2021-04-17 05:58:30 +0300

    Create  The text of the developed library.cs

---
+++ Lab4_tp/The text of the developed library.cs
@@ -0,0 +1,264 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Lab4_tp_bibl_
+{
+ public class RadiusVector
+ {
+ private int x1;
+ private int y1;
+ private double a;
+ public int X1
+ {
+ get { return x1; }
+ set { x1 = value; }
+ }
+ public int Y1
+ {
+ get { return y1; }
+ set { y1 = value; }
+ }
+ public double Vector
+ {
+ get {
+ a = (Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(y1, 2)));
+ return a;
+ }
+ }
+ public RadiusVector()
+ {
+ }
+ public RadiusVector(double vector)
+ {
+ }
+ public RadiusVector(int x1)
+ {
+ this.x1 = x1;
+ }
+ public RadiusVector(int x1, int y1)
+ {
+ this.x1 = x1;
+ this.y1 = y1;
+ }
+ public static explicit operator double(RadiusVector r)
+ {
+ return r.Vector;
+ }
+ public static implicit operator string(RadiusVector r)
+ {
+ string res = "";
+ res += $"{r.Vector}";
+ return res;
+ }
+ private int Compare(RadiusVector r)
+ {
+ if (Vector == r.Vector)
+ return 0;
+ if (Vector > r.Vector)
+ return 1;
+ else
+ return -1;
+ }
+ public static RadiusVector operator +(RadiusVector r, double value)
+ {
+ return new RadiusVector(r.Vector + value);
+ }
+ public static RadiusVector operator -(RadiusVector r, double value)
+ {
+ return new RadiusVector(r.Vector - value);
+ }
+ public static RadiusVector operator *(RadiusVector r, double value)
+ {
+ double b1 = r.x1 * value;
+ double b2 = r.y1 * value;
+ return new RadiusVector((int)Math.Sqrt(Math.Pow(b1, 2) + Math.Pow(b2, 2)));
+ }
+ public static RadiusVector operator /(RadiusVector r, double value)
+ {
+ if(value == 0)
+ {
+ throw new DivideByZeroException();
+ }
+ double b1 = r.x1 * 1 / value;
+ double b2 = r.y1 * 1 / value;
+ return new RadiusVector((int)Math.Sqrt(Math.Pow(b1, 2) + Math.Pow(b2, 2)));
+ }
+ public static bool operator <(RadiusVector first, RadiusVector second)
+ {
+ return first.Compare(second) < 0;
+ }
+ public static bool operator >(RadiusVector first, RadiusVector second)
+ {
+ return first.Compare(second) > 0;
+ }
+ public static bool operator ==(RadiusVector first, RadiusVector second)
+ {
+ return first.Compare(second) == 0;
+ }
+ public static bool operator !=(RadiusVector first, RadiusVector second)
+ {
+ return first.Compare(second) != 0;
+ }
+ public static bool operator <=(RadiusVector first, RadiusVector second)
+ {
+ return first.Compare(second) <= 0;
+ }
+ public static bool operator >=(RadiusVector first, RadiusVector second)
+ {
+ return first.Compare(second) >= 0;
+ }
+ public virtual string GetInfo()
+ {
+ return $"Вектор длинной {Math.Round((Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(y1,
+2))), 3)} c координатами ({x1}, {y1})";
+ }
+ }
+}
+Текст программы
+using System;
+using System.Collections.Generic;
+using Lab4_tp_bibl_;
+namespace Lab04
+{
+ class Program
+ {
+ static void Input_Info(RadiusVector i)
+ {
+ Console.WriteLine("Введите координаты вектора:");
+ Console.Write("x: ");
+ i.X1 = int.Parse(Console.ReadLine());
+ Console.Write("y: ");
+ i.Y1 = int.Parse(Console.ReadLine());
+ }
+ static void Main(string[] args)
+ {
+ RadiusVector vector = new RadiusVector();
+ Input_Info(vector);
+ RadiusVector vector1 = new RadiusVector();
+ Input_Info(vector1);
+ for (; ; )
+ {
+ string[] action;
+ menu:
+ Console.WriteLine("\nДействия: ");
+ Console.WriteLine("'i' Вывести информацию про вектор.\n");
+ Console.WriteLine("'a' Выполнить сложения векторов.\n");
+ Console.WriteLine("'s' Выполнить вычитание векторов.\n");
+ Console.WriteLine("'m' Выполнить умножение вектора на число.\n");
+ Console.WriteLine("'d' Выполнить деление вектора на число.\n");
+ Console.WriteLine("'c' Выполнить сравнение векторов.\n");
+ Console.WriteLine("'ex' Чтобы выйти\n");
+ Console.WriteLine("Введите: ");
+ action = Console.ReadLine().Split();
+ if (action[0] == "i")
+ {
+ Console.WriteLine(vector.GetInfo());
+ Console.WriteLine(vector1.GetInfo());
+ }
+ if (action[0] == "a")
+ {
+ double r = (double)vector + (double)vector1;
+ Console.WriteLine($"Результат: {Math.Round(r, 3)}");
+ }
+ if (action[0] == "s")
+ {
+ double r = (double)vector + (double)vector1;
+ Console.WriteLine($"Результат: {Math.Round(r, 3)}");
+ }
+ if (action[0] == "m")
+ {
+ Console.WriteLine("Выберите вектор:\n");
+ Console.WriteLine($"'1' {vector.GetInfo()} \n");
+ Console.WriteLine($"'2' {vector1.GetInfo()} \n");
+ Console.WriteLine("Введите: ");
+ string v = Console.ReadLine();
+ if (v == "1")
+ {
+ Console.WriteLine("Введите число:");
+ double b = Convert.ToDouble(Console.ReadLine());
+ double r = (double)vector * b;
+ Console.WriteLine($"Результат: {Math.Round(r, 3)}");
+ }
+ if (v == "2")
+ {
+ Console.WriteLine("Введите число:");
+ double c = Convert.ToDouble(Console.ReadLine());
+ double ra = (double)vector1 * c;
+ Console.WriteLine($"Результат: {Math.Round(ra, 3)}");
+ }
+ }
+ if (action[0] == "d")
+ {
+ Console.WriteLine("Выберите вектор:\n");
+ Console.WriteLine($"'1' {vector.GetInfo()} \n");
+ Console.WriteLine($"'2' {vector1.GetInfo()} \n");
+ Console.WriteLine("Введите: ");
+ string va = Console.ReadLine();
+ if (va == "1")
+ {
+ Console.WriteLine("Введите число:");
+ double b = Convert.ToDouble(Console.ReadLine());
+ double r = (double)vector / b;
+ Console.WriteLine($"Результат: {Math.Round(r, 3)}");
+ }
+ if (va == "2")
+ {
+ Console.WriteLine("Введите число:");
+ double c = Convert.ToDouble(Console.ReadLine());
+ double ra = (double)vector1 / c;
+ Console.WriteLine($"Результат: {Math.Round(ra, 3)}");
+ }
+ }
+ if (action[0] == "c")
+ {
+ for (; ; )
+ {
+ Console.WriteLine("\nКакое сравнение вы хотите сделать или введите 'b',
+чтобы вернуться обратно:");
+ Console.WriteLine("Введите: ");
+ string op = Console.ReadLine();
+ if (op == "<")
+ {
+ Console.WriteLine(vector < vector1);
+ Console.WriteLine();
+ }
+ if (op == ">")
+ {
+ Console.WriteLine(vector > vector1);
+ Console.WriteLine();
+ }
+ if (op == "==")
+ {
+ Console.WriteLine(vector == vector1);
+ Console.WriteLine();
+ }
+ if (op == "!=")
+ {
+ Console.WriteLine(vector != vector1);
+ Console.WriteLine();
+ }
+ if (op == "<=")
+ {
+ Console.WriteLine(vector <= vector1);
+ Console.WriteLine();
+ }
+ if (op == ">=")
+ {
+ Console.WriteLine(vector >= vector1);
+ Console.WriteLine();
+ }
+ if (op == "b")
+ {
+ goto menu;
+ }
+ }
+ }
+ if (action[0] == "ex")
+ {
+ break;
+ }
+ }
+ }
+ }
+ }
